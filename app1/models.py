8839df9abb57711158df2cba1e42482492cb5c6a
Ingo Mörl <ingo.moerl@posteo.de>
2022-11-03 08:26:13 +0100

    NIC Computer

--- app1/models.py
+++ app1/models.py
@@ -172,8 +172,9 @@ class NICTyp(models.Model):
 
 class NIC(models.Model):
     typ = models.ForeignKey(NICTyp, on_delete=models.RESTRICT,verbose_name="Typ")
+    computer = models.ForeignKey(Computer, on_delete=models.RESTRICT, verbose_name="Computer", blank=True, null = True)
     mac = models.CharField(max_length=50, verbose_name="MAC Adresse")
-    kommentar =  models.CharField(max_length=50, verbose_name="MAC Adresse", null = True)
+    kommentar =  models.CharField(max_length=50, verbose_name="Kommentar", blank = True)
     def __str__(self):
         return f"NIC{self.id:06} - {self.typ}/[{self.mac}]"
     class Meta:
