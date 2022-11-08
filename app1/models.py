6d6179dd016ae22a4e360c20929eaa83cc07da26
Ingo Mörl <ingo.moerl@posteo.de>
2022-11-08 10:13:53 +0100

    tools erweitert

--- app1/models.py
+++ app1/models.py
@@ -195,8 +195,9 @@ class Tool(models.Model):
     standort = models.ForeignKey(Standort, on_delete=models.RESTRICT,verbose_name="Standort")
     type = models.ForeignKey(ToolTyp, on_delete=models.RESTRICT,verbose_name="Typ")
     # Zusatzfelder
+    bezeichnung = models.CharField(max_length=50, verbose_name="Bezeichnung", blank = True)
     computer = models.ForeignKey(Computer, on_delete=models.RESTRICT, verbose_name="Computer", null = True, blank=True)
-    zusatzinfo = models.CharField(max_length=50, verbose_name="Zusatzinfo", blank = True)
+    zusatzinfo = models.CharField(max_length=250, verbose_name="Zusatzinfo", blank = True)
     def __str__(self):
         return f"T{self.id:08}/{self.type}/{self.standort}"
     class Meta:
