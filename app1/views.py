e0d188ec41898b9741822904f777688baa59a0ab
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2022-11-17 09:53:16 +0100

    Menue und an und abmeldung

--- app1/views.py
+++ app1/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 
 def index(request):
+    if request.method == "POST":
+        print(request.POST)
+        if 'button' in request.POST:
+            if request.POST["button"] == "Anmelden":
+                username = request.POST['login_name']
+                password = request.POST['login_pwd']
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    print("loggin")
+                    login(request, user)
+                else:
+                    print("login failed")
+            elif request.POST["button"] == "Abmelden":
+                logout(request)
+        return redirect("/")         
     liste_computer = Computer.objects.all().order_by('id')
     antwort = []
     for einzel_computer in liste_computer:
